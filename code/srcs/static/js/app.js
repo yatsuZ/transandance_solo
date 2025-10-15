be5b6ee7bbb8aec09594afd01e16fc244ba8d996
yatsuZ <yassine.zaoui28062003@gmail.com>
2025-10-15 02:10:39 +0200

    SPA casser doit reparer

--- code/srcs/static/js/app.js
+++ code/srcs/static/js/app.js
@@ -1,17 +1,8 @@
-const music = document.getElementById('arcade-music');
-const btn = document.getElementById('music-btn');
-
-btn.addEventListener('click', () => {
-  if (music.paused) {
-    music.play();
-  } else {
-    music.pause();
-  }
-});
-
+// SPA et REDIRECTION
 document.addEventListener("DOMContentLoaded", () => {
 
   document.body.addEventListener("click", async (e) => {
+
     const link = e.target.closest("a[data-link]");
     if (!link) return;
 
@@ -35,10 +26,57 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     // anime.js animation
     anime({
-      targets: '#app .container',
+      targets: '#app .container .arcade-header .arcade-title',
+      opacity: [0, 1],
+      translateY: [-20, 0],
+      duration: 500
+    });
+    anime({
+      targets: '#app .container .arcade-header .arcade-subtitle',
       opacity: [0, 1],
       translateY: [-20, 0],
       duration: 500
     });
   });
+});
+
+// Gestion de la music et nav icon
+const music = document.getElementById('arcade-music');
+const iconSound = document.getElementById('icon-sound');
+const iconSettings = document.getElementById('icon-settings');
+const popup = document.getElementById('music-popup');
+const startBtn = document.getElementById('start-music');
+const iconSoundImg = iconSound.querySelector('img');
+
+let isPlaying = localStorage.getItem('isPlaying') === 'true';
+
+// Quand l’utilisateur clique sur le bouton du pop-up
+startBtn.addEventListener('click', () => {
+  music.play().then(() => {
+    isPlaying = true;
+    iconSoundImg.src = './static/util/icon/son_on.png';
+    localStorage.setItem('isPlaying', isPlaying);
+    popup.style.display = 'none'; // on cache le pop-up
+  }).catch(err => console.log("Erreur lecture musique :", err));
+});
+
+// Clic sur icône son pour play/pause
+iconSound.addEventListener('click', () => {
+  if (!isPlaying) {
+    music.play().then(() => {
+      isPlaying = true;
+      iconSoundImg.src = './static/util/icon/son_on.png';
+      localStorage.setItem('isPlaying', isPlaying);
+    }).catch(err => console.log("Erreur lecture musique :", err));
+  } else {
+    music.pause();
+    isPlaying = false;
+    iconSoundImg.src = './static/util/icon/son_off.png';
+    localStorage.setItem('isPlaying', isPlaying);
+  }
+});
+
+// Clic sur engrenage
+iconSettings.addEventListener('click', () => {
+  alert("⚙️ Paramètres à venir !");
 });
